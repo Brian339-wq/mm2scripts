71fc31a874f04908880270f5c787d6b4350fabfe
Brian339-wq <BrianNovokk@gmail.com>
2025-07-07 18:53:15 -0300

    Update Beta Test.lua

--- Beta Test.lua
+++ Beta Test.lua
@@ -1,1 +1,2 @@
 print("Old Version detected, solving it! : mm2 script :..: 003019301x401321")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Brian339-wq/mm2scripts/refs/heads/mm2.lua/mm2%20it.lua"))()
